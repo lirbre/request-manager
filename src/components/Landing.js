21b3cb82238acccf898651087a3709451f1308b2
lirabreno <github.lira@outlook.com>
2021-10-06 22:27:53 -0300

    start project

---
+++ src/components/Landing.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import './App.scss'
+import MenuAppBar from './Navbar'
+
+function Landing() {
+    return (
+        <>
+        <MenuAppBar/>
+        <div>
+            <form>
+                
+            </form>
+        </div>
+        </>
+    )
+}
+
+export default Landing;
